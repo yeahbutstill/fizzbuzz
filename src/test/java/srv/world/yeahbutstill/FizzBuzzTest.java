98b0331b6a14ac9bcb4c971c0d3c53fc3795bae6
Dani Setiawan <mayadani28@gmail.com>
2021-05-14 19:31:23 +0700

    Commit pertama

---
+++ src/test/java/srv/world/yeahbutstill/FizzBuzzTest.java
@@ -0,0 +1,26 @@
+package srv.world.yeahbutstill;
+
+import org.junit.jupiter.api.Test;
+
+public class FizzBuzzTest {
+
+    public void fizzBuzz(int total) {
+        for (int i = 1; i <= total; i++) {
+            if (i % 3 == 0 && i % 5 == 0) {
+                System.out.println("Fizz Buzz");
+            } else if (i % 3 == 0) {
+                System.out.println("Fizz");
+            } else if (i % 5 == 0) {
+                System.out.println("Buzz");
+            } else {
+                System.out.println(i);
+            }
+        }
+    }
+
+    @Test
+    void testFizzBuzz() {
+        fizzBuzz(100);
+    }
+
+}
